7db8fef91e0d86ce6ab18e7fd47044fc1c744d4a
IAGO FILGUEIRAS CHIAPETTA <078463@unifeso.lan>
2025-04-17 12:24:47 -0300

    commit

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ def edit(filename):
     return render_template('edit.html', filename=filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
