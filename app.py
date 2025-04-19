997193cf88f1bc93d4c1664c5a578a358a5fab32
IAGO FILGUEIRAS CHIAPETTA <078463@unifeso.lan>
2025-04-19 16:16:04 -0300

    modo de produção

--- app.py
+++ app.py
@@ -102,4 +102,4 @@ def edit(filename):
     return render_template('edit.html', filename=filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
